43d93291a5f55bfd57824a1bb12fcc8fdbbbb498
xiongshanghua <xiongshanghua@sz.hitrontech.com>
2020-04-14 13:57:41 +0800

    LeetCode7

---
+++ src/main/kotlin/leetcode7/Solution.kt
@@ -0,0 +1,24 @@
+package leetcode7
+
+fun reverse(x: Int): Int {
+    var x1 = x
+    var rev = 0
+    while (x1 != 0) {
+        val pop = x1 % 10
+        x1 /= 10
+        if (rev > Int.MAX_VALUE / 10 || rev == Int.MAX_VALUE / 10 && pop > Int.MAX_VALUE % 10) {
+            rev = 0
+            break
+        } else if (rev < Int.MIN_VALUE / 10 || rev == Int.MIN_VALUE / 10 && x1 < Int.MIN_VALUE % 10) {
+            rev = 0
+            break
+        }
+        rev = rev * 10 + pop
+    }
+    return rev
+}
+
+fun main() {
+    //-2147483648
+    println(reverse(1563847412))
+}

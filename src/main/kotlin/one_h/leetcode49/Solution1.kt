c5d04b939a9f3fbf81f1d7913ad32f8fe5f6c362
xiongshanghua <xiongshanghua@sz.hitrontech.com>
2020-06-04 11:20:41 +0800

    LeetCode49

---
+++ src/main/kotlin/one_h/leetcode49/Solution1.kt
@@ -0,0 +1,24 @@
+package one_h.leetcode49
+
+fun groupAnagrams1(strs: Array<String>): List<List<String>> {
+    val map = HashMap<String, MutableList<String>>()
+
+    strs.forEach {
+        val tmpList = it.toMutableList()
+        tmpList.sort()
+        val key = tmpList.toString()
+        if (!map.containsKey(key)) {
+            map[key] = ArrayList()
+        }
+        map[key]!!.add(it)
+    }
+
+    return ArrayList(map.values)
+}
+
+fun main() {
+    val strs = arrayOf("tea","and","ace","ad","eat","dans")
+    val current = System.currentTimeMillis()
+    println(groupAnagrams1(strs))
+    println("${System.currentTimeMillis() - current} ms")
+}

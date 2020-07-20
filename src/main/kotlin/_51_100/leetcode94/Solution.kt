df4756cace852540d6f6a5c63b0dd5ca0c54cf7f
xiongshanghua <xiongshanghua@sz.hitrontech.com>
2020-07-20 11:41:27 +0800

    LeetCode94

---
+++ src/main/kotlin/_51_100/leetcode94/Solution.kt
@@ -0,0 +1,25 @@
+package _51_100.leetcode94
+
+data class TreeNode(val `val`: Int, var left: TreeNode?, var right: TreeNode?)
+
+val list = ArrayList<Int>()
+
+fun inorderTraversal(root: TreeNode?): List<Int> {
+    if (root?.left != null) {
+        inorderTraversal(root.left)
+    }
+    if (root != null) {
+        list.add(root.`val`)
+    }
+    if (root?.right != null) {
+        inorderTraversal(root.right)
+    }
+
+    return list
+}
+
+fun main() {
+    val root = TreeNode(1, null, TreeNode(2, TreeNode(3, null, null), null))
+
+    println(inorderTraversal(root))
+}

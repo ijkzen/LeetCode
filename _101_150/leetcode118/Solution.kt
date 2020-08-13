9386f09ff006cbe5d8ae9ae4f816c4ffa1ba6af8
ijkzen <ijkzen@outlook.com>
2020-08-13 11:13:16 +0800

    LeetCode118

---
+++ _101_150/leetcode118/Solution.kt
@@ -0,0 +1,25 @@
+package _101_150.leetcode118
+
+fun generate(numRows: Int): List<List<Int>> {
+
+    val result = ArrayList<ArrayList<Int>>()
+    if (0 == numRows) {
+        return result
+    }
+
+    val first = arrayListOf<Int>(1)
+    result.add(first)
+    for (i in 2..numRows) {
+        val list = ArrayList<Int>()
+        val lastList = result[result.size - 1]
+        for (j in 0 until i) {
+            list.add((if (j - 1 < 0) 0 else lastList[j - 1]) + (if (j == lastList.size) 0 else lastList[j]))
+        }
+        result.add(list)
+    }
+    return result
+}
+
+fun main() {
+    println(generate(5))
+}

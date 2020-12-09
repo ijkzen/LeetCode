546ae68ca207d53e0a21caf39c645e9de5e0b4b6
ijkzen <ijkzen@outlook.com>
2020-12-09 14:44:42 +0800

    add new leetcode

---
+++ _800_842/leetcode842/LeetCode842.kt
@@ -0,0 +1,73 @@
+import java.math.BigInteger
+import java.util.*
+
+fun splitIntoFibonacci(fibonacci: String): List<Int> {
+    if (fibonacci.isEmpty() || fibonacci.length < 3) {
+        return arrayListOf()
+    }
+
+    val result = LinkedList<Int>()
+    subSplit(fibonacci, result)
+    return result.reversed().toList()
+}
+
+fun subSplit(fibonacci: String, deque: LinkedList<Int>) {
+    if (fibonacci.isEmpty()) {
+        return
+    }
+
+    val length = fibonacci.length
+    if (deque.isEmpty()) {
+        val maxSubLength = fibonacci.length / 3 + 1
+        for (i in 1..maxSubLength) {
+            val element = fibonacci.substring(length - i, length)
+            if (isValid(element)) {
+                deque.add(element.toInt())
+                subSplit(fibonacci.substring(0, length - i), deque)
+            }
+        }
+
+        if (deque.size < 3) {
+            deque.clear()
+        }
+
+    } else if (deque.size == 1) {
+        val third = deque.first()
+        for (i in 1.."$third".length) {
+            val element = fibonacci.substring(length - i, length)
+            if (isValid(element) && element.toInt() <= third) {
+                deque.add(element.toInt())
+                subSplit(fibonacci.substring(0, length - i), deque)
+            }
+        }
+
+        if (deque.size < 3) {
+            deque.clear()
+        }
+    } else {
+        val dequeLength = deque.size
+        val third = deque[dequeLength - 2]
+        val second = deque[dequeLength - 1]
+
+        val first = "${third - second}"
+
+        if (isValid(first) && fibonacci.endsWith(first)) {
+            deque.add(first.toInt())
+            subSplit(fibonacci.substring(0, length - first.length), deque)
+        } else {
+            deque.removeLast()
+        }
+    }
+}
+
+fun isValid(element: String): Boolean {
+    if (element.length >= 2 && element[0] == '0') {
+        return false
+    }
+
+    return BigInteger(element) < BigInteger("${Int.MAX_VALUE}")
+}
+
+fun main() {
+    println(splitIntoFibonacci("123456579"))
+}
---
+++ _851_900/leetcode861/LeetCode861.kt
@@ -0,0 +1,114 @@
+fun getScore(matrix: Array<IntArray>): Int {
+    if (matrix.isEmpty()) {
+        return 0
+    }
+
+    val rowLength = matrix.size
+    val colLength = matrix[0].size
+
+    for (i in 0 until rowLength) {
+        for (j in 0 until colLength) {
+
+            val numberCol = getNumberForCol(matrix, j)
+            if (numberCol < rowLength - numberCol) {
+                reverseCol(matrix, j)
+            }
+
+            if (matrix[i][0] == 0) {
+                reverseRow(matrix, i)
+            }
+        }
+    }
+
+    return parseMatrix2Score(matrix)
+}
+
+fun parseMatrix2Score(matrix: Array<IntArray>): Int {
+    var value = 0
+    for (i in matrix.indices) {
+        value += parseIntArray2Score(matrix[i])
+    }
+
+    return value
+}
+
+fun parseIntArray2Score(array: IntArray): Int{
+    var value = 0
+    val length = array.size
+    for (i in 0 until length) {
+        value += (array[i] * Math.pow(2.toDouble(), (length - 1 - i).toDouble())).toInt()
+    }
+
+    return value
+}
+
+/**
+ * 获取指定行内数字1的个数
+ */
+fun getNumberForRow(matrix: Array<IntArray>, row: Int): Int {
+    val rowArray = matrix[row]
+    var count = 0
+    for (value in rowArray) {
+        if (value == 1) {
+            count++
+        }
+    }
+
+    return count
+}
+
+/**
+ * 获取指定列内数字1的个数
+ */
+fun getNumberForCol(matrix: Array<IntArray>, col: Int): Int {
+    var count = 0
+    for (i in matrix.indices) {
+        if (matrix[i][col] == 1) {
+            count++
+        }
+    }
+
+    return count
+}
+
+/**
+ * 反转指定行
+ */
+fun reverseRow(matrix: Array<IntArray>, row: Int) {
+    val rowArray = matrix[row]
+
+    for (i in rowArray.indices) {
+        if (rowArray[i] == 0) {
+            rowArray[i] = 1
+        } else {
+            rowArray[i] = 0
+        }
+    }
+}
+
+/**
+ * 反转指定列
+ */
+fun reverseCol(matrix: Array<IntArray>, col: Int) {
+    for (i in matrix.indices) {
+        if (matrix[i][col] == 0) {
+            matrix[i][col] = 1
+        } else {
+            matrix[i][col] = 0
+        }
+    }
+}
+
+fun main() {
+    val matrix = Array<IntArray>(3) {
+        when (it) {
+            0 -> intArrayOf( 0, 1, 1)
+            1 -> intArrayOf(1, 1, 1)
+            2 -> intArrayOf(1, 0, 0)
+            else -> intArrayOf()
+        }
+    }
+
+    println(getScore(matrix))
+}
+

10711aa28f68c4c95754474b7db1e92df0dfbf80
ijkzen <ijkzen@outlook.com>
2020-12-25 11:54:44 +0800

    LeetCode142

---
+++ _101_150/leetcode142/Solution.kt
@@ -0,0 +1,39 @@
+package _101_150.leetcode142
+
+import _50.leetcode2.ListNode
+
+fun detectCycle(head: ListNode?): ListNode? {
+    if (head?.next == null) {
+        return null
+    }
+
+    val set = HashSet<ListNode>()
+
+    var header = head
+
+    while (header!!.next != null) {
+        if (set.contains(header)) {
+            return header
+        } else {
+            set.add(header)
+            header = header.next
+        }
+    }
+
+    return null
+}
+
+fun main() {
+    val node1 = ListNode(3)
+    val node2 = ListNode(2)
+    val node3 = ListNode(0)
+    val node4 = ListNode(-4)
+
+    node1.next = node2
+    node2.next = node3
+    node3.next = node4
+    node4.next = node2
+
+    val header = detectCycle(node1)
+    header
+}

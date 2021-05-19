fc6611782c7437fb843bcb8adfc81b38a5146803
熊尚华 <shanghua.xiong@quanshi.com>
2021-05-19 11:11:00 +0800

    更新 LeetCode 1 运行时间描述

--- _1_50/leetcode1/Solution.kt
+++ _1_50/leetcode1/Solution.kt
@@ -2,6 +2,12 @@ package _1_50.leetcode1
 
 import java.lang.StringBuilder
 
+/**
+ * 2021/05/19 10:56
+ * 运行耗时220ms，超越75%
+ * 排行第一的算法用时184ms，具体算法相似，复制其算法运行，耗时为224ms；
+ * 可能是服务端处理器的更换、负载问题或者测试用例更新导致的
+ */
 
 fun twoSum(nums: IntArray, target: Int): IntArray? {
     val map = mutableMapOf<Int, Int>()

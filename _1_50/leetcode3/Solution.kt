34e91f376a4b97cf8f06fd1cba73ee12badd087d
熊尚华 <shanghua.xiong@quanshi.com>
2021-05-25 14:44:19 +0800

    更新 LeetCode 3 运行时间描述，变更较大值比较算法

--- _1_50/leetcode3/Solution.kt
+++ _1_50/leetcode3/Solution.kt
@@ -3,6 +3,10 @@ package _1_50.leetcode3
 /**
  * @author ijkzen
  * @since 2020/4/11
+ *
+ * 执行耗时216ms，超越95.86%
+ * 原本执行耗时268ms，更改了较大值比较算法；从 kotlin 的 coerceAtLeast 变为 Math.max，
+ * coerceAtLeast 是扩展函数可能有额外的开销；
  */
 fun lengthOfLongestSubstring(s: String): Int {
     var finalLength = 0
@@ -12,10 +16,10 @@ fun lengthOfLongestSubstring(s: String): Int {
     val length = s.length
     while (i < length && j < length) {
         if (map.containsKey(s[j])) {
-            i = (map.getValue(s[j])).coerceAtLeast(i)
+            i = Math.max(i, map[s[j]]!!)
         }
 
-        finalLength = finalLength.coerceAtLeast(j - i + 1)
+        finalLength = Math.max(finalLength, j - i + 1)
         map[s[j]] = ++j
     }
 

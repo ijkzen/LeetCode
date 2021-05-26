4843a9298fb7cca58507d151ed391d6f2007e32f
熊尚华 <shanghua.xiong@quanshi.com>
2021-05-26 16:29:25 +0800

    更新 LeetCode 4 运行时间描述

--- _1_50/leetcode4/Solution.kt
+++ _1_50/leetcode4/Solution.kt
@@ -1,5 +1,10 @@
 package _1_50.leetcode4
 
+/**
+ * 2021/05/26 13:45
+ * 耗时296ms，超越89.44%
+ * 但是我已经看不懂这个算法了，很烦；
+ */
 fun findMedianSortedArrays(nums1: IntArray, nums2: IntArray): Double {
     val a: IntArray
     val b: IntArray
@@ -25,9 +30,9 @@ fun findMedianSortedArrays(nums1: IntArray, nums2: IntArray): Double {
     while (imin <= imax) {
         i = (imin + imax) / 2
         j = half - i
-        if (i < imax && b[j - 1] > a[i] ) {
+        if (i < imax && b[j - 1] > a[i]) {
             imin = i + 1
-        } else if ( i > imin && a[i - 1] > b[j] ) {
+        } else if (i > imin && a[i - 1] > b[j]) {
             imax = i - 1
         } else {
             val maxLeft: Int = when {
@@ -38,7 +43,7 @@ fun findMedianSortedArrays(nums1: IntArray, nums2: IntArray): Double {
                     a[i - 1]
                 }
                 else -> {
-                    a[i - 1].coerceAtLeast(b[j - 1])
+                    Math.max(a[i - 1], b[j - 1])
                 }
             }
 
@@ -54,7 +59,7 @@ fun findMedianSortedArrays(nums1: IntArray, nums2: IntArray): Double {
                     a[i]
                 }
                 else -> {
-                    b[j].coerceAtMost(a[i])
+                    Math.min(b[j], a[i])
                 }
             }
 
@@ -66,8 +71,8 @@ fun findMedianSortedArrays(nums1: IntArray, nums2: IntArray): Double {
 }
 
 fun main() {
-    val a = intArrayOf(1, 2, 3, 4, 5)
-    val b = intArrayOf(6, 7, 8)
+    val a = intArrayOf(1, 2, 4)
+    val b = intArrayOf(3, 5, 6, 7)
 
     println(findMedianSortedArrays(a, b))
 }

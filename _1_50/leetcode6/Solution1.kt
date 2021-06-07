7ce9ce8ce98bf4caf74fb90b1d7084334a4bd1d3
熊尚华 <shanghua.xiong@quanshi.com>
2021-06-07 13:40:43 +0800

    修改 leetcode 6 部分逻辑

--- _1_50/leetcode6/Solution1.kt
+++ _1_50/leetcode6/Solution1.kt
@@ -3,7 +3,7 @@ package _1_50.leetcode6
 import java.lang.StringBuilder
 
 /**
- * 运行时间224ms，超越98.31%
+ * 运行时间216ms，超越100%
  * 时间复杂度 O(n) = n，直接构造了通项，所以要快许多；
  */
 fun convert1(s: String, numRows: Int): String {
@@ -11,18 +11,20 @@ fun convert1(s: String, numRows: Int): String {
         return s
     }
 
-    val builder = StringBuilder()
+    val result = CharArray(s.length)
+    var index = 0
     val internal = numRows - 1
     for (i in 0 until numRows) {
         var j = i
 
         while (j < s.length) {
-            builder.append(s[j])
+            result[index] = s[j]
             j += 2 * (internal - j % internal)
+            index++
         }
     }
 
-    return builder.toString()
+    return String(result)
 }
 
 fun main() {

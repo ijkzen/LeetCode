41b4c83004d394fa2d43fb7e5bb0cf816f131343
熊尚华 <shanghua.xiong@quanshi.com>
2021-06-21 14:32:45 +0800

    添加 leetcode 11 运行描述

--- _1_50/leetcode11/Solution.kt
+++ _1_50/leetcode11/Solution.kt
@@ -1,5 +1,9 @@
 package _1_50.leetcode11
 
+/**
+ * 执行耗时:496 ms,击败了45.67% 的Kotlin用户
+ * 内存消耗:48.3 MB,击败了65.35% 的Kotlin用户
+ */
 fun maxArea(height: IntArray): Int {
     var result = 0
     var i = 0
@@ -7,7 +11,7 @@ fun maxArea(height: IntArray): Int {
     while (i < j) {
         val a = height[i]
         val b = height[j]
-        val tmp = a.coerceAtMost(b) * (j - i)
+        val tmp = Math.min(a, b) * (j - i)
         if (tmp > result) {
             result = tmp
         }

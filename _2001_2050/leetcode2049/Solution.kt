8c20a85e696c448396707d3e199cca84a7dd7d84
ijkzen <ijkzen@outlook.com>
2022-04-04 23:40:49 +0800

    improve LeetCode 2049

--- _2001_2050/leetcode2049/Solution.kt
+++ _2001_2050/leetcode2049/Solution.kt
@@ -1,14 +1,14 @@
 package _2001_2050.leetcode2049
 
 /**
- * Your runtime beats 18.52 % of kotlin submissions
- * Your memory usage beats 37.04 % of kotlin submissions (69.5 MB)
+ * Your runtime beats 25.93 % of kotlin submissions
+ * Your memory usage beats 70.37 % of kotlin submissions (61.5 MB)
  */
 data class TreeNode(
     var left: TreeNode? = null,
-    var right: TreeNode? = null,
+    var right: TreeNode? = null
 ) {
-    var size: Long = 0
+    var size: Long = 0L
         get() {
             if (field != 0L){
                 return field
@@ -19,51 +19,60 @@ data class TreeNode(
                 return field
             }
         }
+
+    fun addChild(child: TreeNode) {
+        if (left == null) {
+            left = child
+        } else {
+            right = child
+        }
+    }
 }
 
-
 fun countHighestScoreNodes(parents: IntArray): Int {
-    // 下标 --> 子树 的映射
-    val treeNodeMap = hashMapOf<Int, TreeNode>()
-    for (i in parents.indices) {
-        treeNodeMap[i] = TreeNode()
+    val treeNodeArray = Array<TreeNode>(parents.size) {
+        TreeNode()
     }
 
     for (i in 1 until parents.size) {
-        val parentNode = treeNodeMap[parents[i]]
-        val childNode = treeNodeMap[i]
-        putChild2Tree(parentNode!!, childNode!!)
+        val parentNode = treeNodeArray[parents[i]]
+        val childNode = treeNodeArray[i]
+        parentNode.addChild(childNode)
     }
 
-    treeNodeMap[0]!!.size
+    // 递归初始化 size 属性
+    treeNodeArray[0].size
 
-    val resultList = mutableListOf<Long>()
+    var maxValue = 0L
+    var maxCount = 0
     for (i in parents.indices) {
-        val rootNode = treeNodeMap[0]!!
+        val rootNode = treeNodeArray[0]
         if (parents[i] == -1) {
             val leftSize  :Long = rootNode.left?.size ?: 1L
             val rightSize :Long = rootNode.right?.size ?: 1L
             val result = leftSize * rightSize
-            resultList.add(result)
+            if (result > maxValue) {
+                maxValue = result
+                maxCount = 1
+            } else if (result == maxValue) {
+                maxCount++
+            }
         } else {
-            val childNode = treeNodeMap[i]!!
+            val childNode = treeNodeArray[i]
             val rootSize = rootNode.size - childNode.size
             val childLeftSize = childNode.left?.size ?: 1
             val childRightSize = childNode.right?.size ?: 1
-            resultList.add(childLeftSize * childRightSize * rootSize)
+            val result = childLeftSize * childRightSize * rootSize
+            if (result > maxValue) {
+                maxValue = result
+                maxCount = 1
+            } else if (result == maxValue) {
+                maxCount++
+            }
         }
     }
 
-    val max = resultList.maxOrNull() ?: 0
-    return resultList.filter { it == max }.size
-}
-
-fun putChild2Tree(parent: TreeNode, child: TreeNode) {
-    if (parent.left == null) {
-        parent.left = child
-    } else {
-        parent.right = child
-    }
+    return maxCount
 }
 
 fun main() {
